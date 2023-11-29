1aa369f82f331598fe3e8691f2483ff5e2820eec
Tomas Martinez <148986770+Genoss8@users.noreply.github.com>
2023-11-29 04:24:47 +0000

    programa finalizado

---
+++ Ejemplo12/programa12/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int min = 1;
+        int max = 100;
+        int r = 0;
+        Random rnd = new Random();
+        r = rnd.Next(min, max + 1);
+        Console.WriteLine(r);
+         if (r <= 50)
+         {
+            Console.WriteLine("Se le asignara al grupo 1");
+         }
+         else
+         {
+            Console.WriteLine("Se le asignara al grupo 2 ");
+
+         }
+    }
+}
