7070ffa1f260bb49857adfcaf724b4a8df6b4cae
Tomas Martinez <148986770+Genoss8@users.noreply.github.com>
2023-11-30 02:09:47 +0000

    programa terminado

---
+++ ejemplo1/ejemplo1/Program.cs
@@ -0,0 +1,14 @@
+﻿decimal precioProd;
+string producto;
+int cant;
+decimal importP;
+decimal cambio;
+
+Console.WriteLine("Ingrese nombre del producto");
+producto=(Console.ReadLine());
+Console.WriteLine("Ingrese precio del producto");
+precioProd=decimal.Parse(Console.ReadLine());
+Console.WriteLine("La cantidad total del producto es?");
+cant=int.Parse(Console.ReadLine());
+importP= precioProd*cant;
+Console.WriteLine("su importe total a pagar es:"+ importP);
