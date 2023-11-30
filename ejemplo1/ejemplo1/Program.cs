4118ba6f32b3a73f3dbb30cff5d943bf4331a1f6
Tomas Martinez <148986770+Genoss8@users.noreply.github.com>
2023-11-30 02:26:02 +0000

    finalizado

--- ejemplo1/ejemplo1/Program.cs
+++ ejemplo1/ejemplo1/Program.cs
@@ -3,7 +3,7 @@ string producto;
 int cant;
 decimal importP;
 decimal cambio;
-
+decimal pago;
 Console.WriteLine("Ingrese nombre del producto");
 producto=(Console.ReadLine());
 Console.WriteLine("Ingrese precio del producto");
@@ -12,3 +12,7 @@ Console.WriteLine("La cantidad total del producto es?");
 cant=int.Parse(Console.ReadLine());
 importP= precioProd*cant;
 Console.WriteLine("su importe total a pagar es:"+ importP);
+Console.WriteLine("El cliente pago un total de:");
+pago=decimal.Parse(Console.ReadLine());
+cambio=Math.Abs(importP-pago);
+Console.WriteLine("su cambio es:"+cambio);
