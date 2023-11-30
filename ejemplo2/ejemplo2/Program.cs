847239924c3d2ef802b6af18fb5d089e252e60a8
Tomas Martinez <148986770+Genoss8@users.noreply.github.com>
2023-11-30 03:17:17 +0000

    programa finalizado

---
+++ ejemplo2/ejemplo2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+double calif1;
+double calif2;
+double calif3;
+double suma;
+double prom;
+Console.WriteLine("calificacion 1 del alumno es:");
+calif1=double.Parse(Console.ReadLine());
+Console.WriteLine("calificacion 2 del alumno es:");
+calif2=double.Parse(Console.ReadLine());
+Console.WriteLine("calificacion 3 del alumno es:");
+calif3=double.Parse(Console.ReadLine());
+suma=calif1+calif2+calif3;
+prom=suma/3;
+Console.WriteLine("El promedio del alumno es:"+prom);
