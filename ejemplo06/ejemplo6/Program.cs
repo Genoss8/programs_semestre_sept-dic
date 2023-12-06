6739e528cd63b88ee84d463ff57f6379fddc27ac
Tomas Martinez <148986770+Genoss8@users.noreply.github.com>
2023-12-06 07:48:43 +0000

    programa terminado

---
+++ ejemplo06/ejemplo6/Program.cs
@@ -0,0 +1,14 @@
+﻿string nombre;
+int edad;
+Console.WriteLine("escriba su nombre");
+nombre=(Console.ReadLine());
+Console.WriteLine("indique su edad");
+edad=int.Parse(Console.ReadLine());
+    if (edad<=18)
+{
+    Console.WriteLine("puedes votar");
+    }
+    else
+    {
+    Console.WriteLine("debes esperar.....hasta luego   "+nombre);
+    }
