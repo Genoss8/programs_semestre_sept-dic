f746a85d7837217a43a0623b26658ee3cc72bc8f
Tomas Martinez <148986770+Genoss8@users.noreply.github.com>
2023-12-07 08:44:09 +0000

    falta checar orden

---
+++ ejemplo10/ejemplo10/Program.cs
@@ -0,0 +1,18 @@
+﻿int clientesDia=0;
+string nombre;
+int clientes=0;
+Console.WriteLine("ingrese el nombre del siguiente cliente:");
+nombre=Console.ReadLine();
+if (clientes==0)
+{
+    clientes=1;
+}
+
+if (clientesDia==0)
+{
+    clientesDia=1;
+}
+clientesDia++;
+Console.WriteLine("El numero de clientes del dia es:{0}"+clientesDia);
+Console.WriteLine("El numero total de clientes es:{0}"+clientes);
+
