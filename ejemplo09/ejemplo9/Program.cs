46edf08b2b3e8848310b83091ae6c3837886c090
Tomas Martinez <148986770+Genoss8@users.noreply.github.com>
2023-12-07 08:17:16 +0000

    programa terminado

--- ejemplo09/ejemplo9/Program.cs
+++ ejemplo09/ejemplo9/Program.cs
@@ -1,8 +1,11 @@
-﻿int numCliente;
-int opcion;
+﻿string nombre;
 int contador=0;
-Console.WriteLine("Se registrara el cliente?");
-if (opcion=1)
+Console.WriteLine("ingrese nombre del  cliente:");
+nombre=Console.ReadLine();  
+while (nombre!="")
 {
-    
+    contador=contador+1;
+    Console.WriteLine("ingrese el nombre del siguiente cliente:");
+    nombre=Console.ReadLine();
+    Console.WriteLine("El numero de clientes es:"+ contador);
 }
